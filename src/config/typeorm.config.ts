c9fd1017842a12529f1662c458b0095081d9afb3
Gian Carlo Mantuan Dala Rosa <giancarlo.mdr@gmail.com>
2021-09-15 11:30:40 -0300

    Update typeorm.config.ts

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -5,7 +5,7 @@ export const typeormConfig: TypeOrmModuleOptions = {
   host: '172.20.0.2',
   port: 5432,
   username: 'postgres',
-  password: 't3cn0sp33d@00550',
+  password: 'password',
   database: 'taskmanagement',
   autoLoadEntities: true,
   synchronize: true,
